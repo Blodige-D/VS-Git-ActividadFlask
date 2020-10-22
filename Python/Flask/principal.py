c6470499e917a7c91e43742648d1ea9ecde8ac73
unknown <you@example.com>
2020-10-21 23:58:37 -0500

    ya terminando

--- Python/Flask/principal.py
+++ Python/Flask/principal.py
@@ -22,5 +22,39 @@ def Diego3():
 def Diego4():
     return render_template("Diego/DatosTecnologicos.html")
 
+
+@app.route("/David")
+def David1():
+    return render_template("David/PRINCIPAL.html")
+
+@app.route("/David/DatosBasicos")
+def David2():
+    return render_template("David/DATOS PERSONALES.html")
+
+@app.route("/David/DatosAcademicos")
+def David3():
+    return render_template("David/FORMACION.html")
+
+@app.route("/David/DatosTecnologicos")
+def David4():
+    return render_template("David/TECNOLOGIAS.html")
+
+
+@app.route("/Fabian")
+def Fabian1():
+    return render_template("Fabian/principal.html")
+
+@app.route("/Fabian/DatosBasicos")
+def Fabian2():
+    return render_template("Fabian/datosB.html")
+
+@app.route("/Fabian/DatosAcademicos")
+def Fabian3():
+    return render_template("Fabian/datosA.html")
+
+@app.route("/Fabian/DatosTecnologicos")
+def Fabian4():
+    return render_template("Fabian/datosT.html")
+
 if __name__ == "__main__":
     app.run(debug = True)
