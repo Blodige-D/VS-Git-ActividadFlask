f94940b0651055db259e3b20e94adfe15e602d94
unknown <you@example.com>
2020-10-21 23:29:38 -0500

    avanzando

--- Python/Flask/principal.py
+++ Python/Flask/principal.py
@@ -5,7 +5,22 @@ app = Flask(__name__)
 @app.route("/")
 def Menu():
     return render_template("index.html")
-    
+
+@app.route("/Diego")
+def Diego1():
+    return render_template("Diego/Inicio.html")
+
+@app.route("/Diego/DatosBasicos")
+def Diego2():
+    return render_template("Diego/DatosBasicos.html")
+
+@app.route("/Diego/DatosAcademicos")
+def Diego3():
+    return render_template("Diego/DatosAcademicos.html")
+
+@app.route("/Diego/DatosTecnologicos")
+def Diego4():
+    return render_template("Diego/DatosTecnologicos.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
